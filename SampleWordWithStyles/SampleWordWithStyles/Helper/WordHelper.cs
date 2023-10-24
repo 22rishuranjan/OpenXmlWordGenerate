f5e386a5402954008f320ff6de474647afc01cb7
22rishuranjan <22rishuranjan@gmail.com>
2023-10-24 23:48:06 +0800

    added files

---
+++ SampleWordWithStyles/SampleWordWithStyles/Helper/WordHelper.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace SampleWordWithStyles.Helper
+{
+	public static class WordHelper
+	{
+
+
+        public static Body GetBodyOfAWordDocument(string filepath)
+
+        {
+            // Open a WordprocessingDocument for editing using the filepath.
+            WordprocessingDocument wordprocessingDocument =
+                WordprocessingDocument.Open(filepath, true);
+
+            // Assign a reference to the existing document body.
+            return wordprocessingDocument.MainDocumentPart?.Document?.Body;
+
+            // Add text to paragraoh.
+
+
+
+
+            // Add Custom Attribute
+            Paragraph para
+            if (attribute != null)
+                Paragraph para = AddTextToParagraph(text, attribute);
+
+            //Add paragraph to the body
+            body?.AppendChild<Paragraph>(para);
+
+            // Close the handle explicitly.
+            wordprocessingDocument.Dispose();
+        }
+
+
+        public static Paragraph AddTextToParagraph(string text)
+        {
+            Paragraph para = new Paragraph();
+            Run run = new Run();
+            run.AppendChild(new Text(text));
+            para.AppendChild<Run>(run);
+            return para;
+        }
+
+        public static Paragraph AddTextToParagraph(string text, OpenXmlAttribute attribute)
+        {
+            Paragraph para = new Paragraph();
+            Run run = new Run();
+            run.AppendChild(new Text(text));
+            para.AppendChild<Run>(run);
+
+            AddCustomAttribute(para, new OpenXmlAttribute("Id", "", "new para")));
+
+            return para;
+        }
+
+        public static void AddCustomAttribute(OpenXmlElement elem, OpenXmlAttribute attr)
+        {
+            elem.SetAttribute(attr);
+        }
+
+
+
+
+        // Apply a style to a paragraph.
+        public static void ApplyStyleToParagraph(WordprocessingDocument doc,
+
+            string styleid, string stylename, Paragraph p)
+        {
+            // If the paragraph has no ParagraphProperties object, create one.
+            if (p.Elements<ParagraphProperties>().Count() == 0)
+            {
+                p.PrependChild<ParagraphProperties>(new ParagraphProperties());
+            }
+
+            // Get the paragraph properties element of the paragraph.
+            ParagraphProperties pPr = p.Elements<ParagraphProperties>().First();
+
+            // Get the Styles part for this document.
+            StyleDefinitionsPart part =
+                doc.MainDocumentPart.StyleDefinitionsPart;
+
+            // If the Styles part does not exist, add it and then add the style.
+            if (part == null)
+            {
+                part = AddStylesPartToPackage(doc);
+                AddNewStyle(part, styleid, stylename);
+            }
+            else
+            {
+                // If the style is not in the document, add it.
+                if (IsStyleIdInDocument(doc, styleid) != true)
+                {
+                    // No match on styleid, so let's try style name.
+                    string styleidFromName = GetStyleIdFromStyleName(doc, stylename);
+                    if (styleidFromName == null)
+                    {
+                        AddNewStyle(part, styleid, stylename);
+                    }
+                    else
+                        styleid = styleidFromName;
+                }
+            }
+
+            // Set the style of the paragraph.
+            pPr.ParagraphStyleId = new ParagraphStyleId() { Val = styleid };
+        }
+
+        // Return true if the style id is in the document, false otherwise.
+        public static bool IsStyleIdInDocument(WordprocessingDocument doc,
+            string styleid)
+        {
+            // Get access to the Styles element for this document.
+            Styles s = doc.MainDocumentPart.StyleDefinitionsPart.Styles;
+
+            // Check that there are styles and how many.
+            int n = s.Elements<Style>().Count();
+            if (n == 0)
+                return false;
+
+            // Look for a match on styleid.
+            Style style = s.Elements<Style>()
+                .Where(st => (st.StyleId == styleid) && (st.Type == StyleValues.Paragraph))
+                .FirstOrDefault();
+
+            if (style == null)
+                return false;
+
+            return true;
+        }
+
+        // Return styleid that matches the styleName, or null when there's no match.
+        public static string GetStyleIdFromStyleName(WordprocessingDocument doc, string styleName)
+        {
+            StyleDefinitionsPart stylePart = doc.MainDocumentPart.StyleDefinitionsPart;
+            string styleId = stylePart.Styles.Descendants<StyleName>()
+                .Where(s => s.Val.Value.Equals(styleName) &&
+                    (((Style)s.Parent).Type == StyleValues.Paragraph))
+                .Select(n => ((Style)n.Parent).StyleId).FirstOrDefault();
+            return styleId;
+        }
+
+        // Create a new style with the specified styleid and stylename and add it to the specified
+        // style definitions part.
+        private static void AddNewStyle(StyleDefinitionsPart styleDefinitionsPart,
+            string styleid, string stylename)
+        {
+            // Get access to the root element of the styles part.
+            Styles styles = styleDefinitionsPart.Styles;
+
+            // Create a new paragraph style and specify some of the properties.
+            Style style = new Style()
+            {
+                Type = StyleValues.Paragraph,
+                StyleId = styleid,
+                CustomStyle = true
+            };
+            StyleName styleName1 = new StyleName() { Val = stylename };
+            BasedOn basedOn1 = new BasedOn() { Val = "Normal" };
+            NextParagraphStyle nextParagraphStyle1 = new NextParagraphStyle() { Val = "Normal" };
+            style.Append(styleName1);
+            style.Append(basedOn1);
+            style.Append(nextParagraphStyle1);
+
+            // Create the StyleRunProperties object and specify some of the run properties.
+            StyleRunProperties styleRunProperties1 = new StyleRunProperties();
+            Bold bold1 = new Bold();
+            Color color1 = new Color() { ThemeColor = ThemeColorValues.Accent2 };
+            RunFonts font1 = new RunFonts() { Ascii = "Lucida Console" };
+            Italic italic1 = new Italic();
+            // Specify a 12 point size.
+            FontSize fontSize1 = new FontSize() { Val = "24" };
+            styleRunProperties1.Append(bold1);
+            styleRunProperties1.Append(color1);
+            styleRunProperties1.Append(font1);
+            styleRunProperties1.Append(fontSize1);
+            styleRunProperties1.Append(italic1);
+
+            // Add the run properties to the style.
+            style.Append(styleRunProperties1);
+
+            // Add the style to the styles part.
+            styles.Append(style);
+        }
+
+        // Add a StylesDefinitionsPart to the document.  Returns a reference to it.
+        public static StyleDefinitionsPart AddStylesPartToPackage(WordprocessingDocument doc)
+        {
+            StyleDefinitionsPart part;
+            part = doc.MainDocumentPart.AddNewPart<StyleDefinitionsPart>();
+            Styles root = new Styles();
+            root.Save(part);
+            return part;
+        }
+
+    }
+}
+
---
+++ SampleWordWithStyles/SampleWordWithStyles/Program.cs
@@ -0,0 +1,86 @@
+﻿
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using SampleWordWithStyles.Helper;
+
+namespace SampleWordWithStyles;
+
+class Program
+{
+    public static string filePath = @"/Volumes/badmonster disk/Development/MyGit/dotnet/OpenXmlWordGenerate/SampleWord/SampleWord/Files/Doc1.docx";
+
+
+    static void Main(string[] args)
+    {
+
+
+        
+
+
+        using (WordprocessingDocument doc =
+            WordprocessingDocument.Open(filePath, true))
+        {
+
+            ApplyStyleToSpecificPara(doc);
+
+            //// Get the first paragraph.
+            //Paragraph p =
+            //  doc.MainDocumentPart.Document.Body.Descendants<Paragraph>()
+            //  .ElementAtOrDefault(1);
+
+            //// Check for a null reference. 
+            //if (p == null)
+            //{
+            //    throw new ArgumentOutOfRangeException("p",
+            //        "Paragraph was not found.");
+            //}
+
+            //WordHelper.ApplyStyleToParagraph(doc, "OverdueAmount", "Overdue Amount", p);
+        }
+        Console.WriteLine("Hello, World!");
+    }
+
+
+    public static void AddTextToWord(string text)
+    {
+        var wordprocessingDocument = WordHelper.GetAWordDocument(filePath);
+        var body = wordprocessingDocument.MainDocumentPart?.Document?.Body;
+        Paragraph para = WordHelper.AddTextToParagraph("Adding paragraph to the document");
+        body?.AppendChild<Paragraph>(para);
+        wordprocessingDocument.Dispose();
+    }
+
+    public static void AddTextToWord(string text, Paragraph para)
+    {
+        var wordprocessingDocument = WordHelper.GetAWordDocument(filePath);
+        var body = wordprocessingDocument.MainDocumentPart?.Document?.Body;
+        body?.AppendChild<Paragraph>(para);
+        wordprocessingDocument.Dispose();
+    }
+
+    public static void ApplyStyleToSpecificPara(WordprocessingDocument doc)
+    {
+
+        // Get all paragraph.
+        var paraList = doc.MainDocumentPart.Document.Body.Descendants<Paragraph>();
+
+        foreach(Paragraph p in paraList)
+        {
+            if (p.HasAttributes)
+            {
+                var attrs = p.GetAttributes();
+                foreach(var attr in attrs)
+                {
+                    if (attr.LocalName == "Id" && attr.Value == "new para")
+                    {
+                        WordHelper.ApplyStyleToParagraph(doc, "OverdueAmount", "Overdue Amount", p);
+                    }
+
+                }
+
+               
+            }
+        }
+        
+    }
+}
